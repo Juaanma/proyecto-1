673afae287dc64ada741fa235f29a7849dc314b5
Juaanma <juanmanuelsuarez10@gmail.com>
2020-04-25 15:54:01 -0300

    Agrego Bootstrap y creo estructura básica

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
+// N = poblacion total
 
+// S = susceptibles
+// I = infectados
+// R = recuperados / removidos (ver terminologia)
+
+// b = tasa de transmision
+// g = tasa de .. (ver terminologia)
+
+// S + I + R = N
+
+// S'(t) = -bSI
+// I'(t) = bSI - gI
+// R'(t) = gI
