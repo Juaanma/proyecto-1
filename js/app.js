715737701422735dcf8356ebd9582504fac0b839
Juaanma <juanmanuelsuarez10@gmail.com>
2020-04-30 16:57:54 -0300

    Agrego navegación

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
-// Seleccionamos sección por defecto en caso de que URL no especifique
-if (window.location.hash == '') {
-    window.location.hash = '#intro';
+// Selecciona sección por defecto en caso que la actual no sea válida
+function checkCurrentSection() {
+    let section = window.location.hash;
+
+    let sectionLinks = $(`.nav-header a[href="${section}"]`);
+    if (sectionLinks.length == 0) {
+        window.location.hash = '#intro';
+    }
 }
+
+// Actualiza interfaz para mostrar sección actual en nav-header
+function updateSectionLinks() {
+    let sectionLinks = $('.nav-header a');
+    sectionLinks.removeClass('active');
+
+    let section = window.location.hash;
+    let sectionNavLink = sectionLinks.filter(`[href="${section}"]`);
+    sectionNavLink.addClass('active');
+}
+
+checkCurrentSection();
+updateSectionLinks();
+
+// Verifica sección y actualiza links al cambiar de sección
+$(window).on('hashchange', function() {
+    checkCurrentSection();
+    updateSectionLinks();
+});
 
 // N = poblacion total
 
