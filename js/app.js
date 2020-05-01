651e2a4bfd1a92948e2457b42fba46d590167810
Juaanma <juanmanuelsuarez10@gmail.com>
2020-04-30 23:54:04 -0300

    Descripción básica del modelo

--- js/app.js
+++ js/app.js
@@ -26,18 +26,3 @@ $(window).on('hashchange', function() {
     checkCurrentSection();
     updateSectionLinks();
 });
-
-// N = poblacion total
-
-// S = susceptibles
-// I = infectados
-// R = recuperados / removidos (ver terminologia)
-
-// b = tasa de transmision
-// g = tasa de .. (ver terminologia)
-
-// S + I + R = N
-
-// S'(t) = -bSI
-// I'(t) = bSI - gI
-// R'(t) = gI
